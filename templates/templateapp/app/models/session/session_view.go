0ec638b400780dd9edc5c22e1d66089f470b2a0c
Adilson Chacon <adilsonchacon@gmail.com>
2020-07-24 23:56:33 +0000

    bug fix for invalid session mailer template path

--- templates/templateapp/app/models/session/session_view.go
+++ templates/templateapp/app/models/session/session_view.go
@@ -55,7 +55,7 @@ func SignUpMailer(currentUser *entities.User) string {
 
 	data := SignUpSuccess{UserFirstName: user.FirstName(currentUser), AppName: config.App.AppName}
 
-	tmpl, err := template.ParseFiles("./app/session/mailer/sign_up." + currentUser.Locale + ".html")
+	tmpl, err := template.ParseFiles("./app/models/session/mailer/sign_up." + currentUser.Locale + ".html")
 	if err != nil {
 		log.Error.Println(err)
 	}
@@ -70,7 +70,7 @@ func PasswordRecoveryInstructionsMailer(currentUser *entities.User, token string
 
 	data := PasswordRecoveryInstructions{UserFirstName: user.FirstName(currentUser), LinkToPasswordRecovery: config.App.ResetPasswordUrl + "?token=" + token}
 
-	tmpl, err := template.ParseFiles("./app/session/mailer/password_recovery." + currentUser.Locale + ".html")
+	tmpl, err := template.ParseFiles("./app/models/session/mailer/password_recovery." + currentUser.Locale + ".html")
 	if err != nil {
 		log.Error.Println(err)
 	}
